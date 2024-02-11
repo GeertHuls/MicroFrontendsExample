489fd6baefe21e1ee3c57333629b6acd78a29e28
Geert Huls <geert.huls82@gmail.com>
2024-02-11 22:29:57 +0100

    Load child projects into shell project

---
+++ Shell/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'payment',
+    loadChildren: () => import('../../../payment/src/app/payment/payment.module').then(m => m.PaymentModule)
+  },
+  {
+    path: 'billing',
+    loadChildren: () => import('../../../billing/src/app/billing/billing.module').then(m => m.BillingModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
